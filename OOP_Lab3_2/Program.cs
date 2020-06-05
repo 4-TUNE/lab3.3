53bf186273e9757b676711d254d60b849b365f1a
4-TUNE <61144719+4-TUNE@users.noreply.github.com>
2020-06-05 12:10:01 +0300

    Добавьте файлы проекта.

---
+++ OOP_Lab3_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OOP_Lab3_2
+{
+    class Program
+    {
+        static string fileRead = @"input.txt";
+
+        static string fileWrite = @"output.txt";
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(fileRead);
+            StreamWriter writer = new StreamWriter(fileWrite, false, System.Text.Encoding.Default);
+
+            string read = reader.ReadToEnd();
+
+            read = read.Replace(" ", "");
+            writer.Write(read);
+            writer.Close();
+
+        }
+    }
+}
